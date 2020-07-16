551dad49e56bb750cb96f74697522bbf9c988fe6
Jeff MAURY <jmaury@redhat.com>
2020-07-16 14:19:35 +0200

    Switch to server 0.2.2
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/org/jboss/tools/intellij/analytics/AnalyticsProcessStreamConnectionProvider.java
+++ src/main/java/org/jboss/tools/intellij/analytics/AnalyticsProcessStreamConnectionProvider.java
@@ -11,7 +11,7 @@ public class AnalyticsProcessStreamConnectionProvider extends ProcessStreamConne
   @Override
   public ProcessBuilder createProcessBuilder() {
     ProcessBuilder builder = super.createProcessBuilder();
-    builder.environment().put("RECOMMENDER_API_URL", "https://f8a-analytics-2445582058137.production.gw.apicast.io:443/api/v1");
+    builder.environment().put("RECOMMENDER_API_URL", "https://f8a-analytics-2445582058137.production.gw.apicast.io:443/api/v2");
     builder.environment().put("THREE_SCALE_USER_TOKEN", "9e7da76708fe374d8c10fa752e72989f");
     return builder;
   }
