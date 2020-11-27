d50e780bedc9f5c2b63b5da2d20c87896f0438b9
Jeff MAURY <jmaury@redhat.com>
2020-11-27 14:49:40 +0100

    Switch to ca-server 0.4.4 and remove UUID
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/org/jboss/tools/intellij/analytics/AnalyticsProcessStreamConnectionProvider.java
+++ src/main/java/org/jboss/tools/intellij/analytics/AnalyticsProcessStreamConnectionProvider.java
@@ -4,7 +4,6 @@ import org.wso2.lsp4intellij.client.connection.ProcessStreamConnectionProvider;
 
 import java.io.File;
 import java.util.List;
-import java.util.UUID;
 
 public class AnalyticsProcessStreamConnectionProvider extends ProcessStreamConnectionProvider {
   public AnalyticsProcessStreamConnectionProvider(List<String> commands, String workingDir) {
@@ -17,7 +16,6 @@ public class AnalyticsProcessStreamConnectionProvider extends ProcessStreamConne
     builder.redirectError(ProcessBuilder.Redirect.INHERIT);
     builder.environment().put("RECOMMENDER_API_URL", "https://f8a-analytics-2445582058137.production.gw.apicast.io:443/api/v2");
     builder.environment().put("THREE_SCALE_USER_TOKEN", "9e7da76708fe374d8c10fa752e72989f");
-    builder.environment().put("UUID", UUID.randomUUID().toString());
     //builder.environment().put("PROVIDE_FULLSTACK_ACTION", "true");
     return builder;
   }
