51f947720e981df9dc0589eb2abb2d38519fb7e5
Jeff MAURY <jmaury@redhat.com>
2020-11-30 08:07:17 +0100

    Switch to ballerina-platform/lsp4intellij library (#24)
    * Switch to ballerina-platform/lsp4intellij library
    
    Fixes #23
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/org/jboss/tools/intellij/analytics/AnalyticsPreloadActivity.java
+++ src/main/java/org/jboss/tools/intellij/analytics/AnalyticsPreloadActivity.java
@@ -1,39 +1,25 @@
 package org.jboss.tools.intellij.analytics;
 
-import com.github.gtache.lsp.client.languageserver.serverdefinition.LanguageServerDefinition;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
-import com.intellij.ide.plugins.cl.PluginClassLoader;
 import com.intellij.openapi.components.BaseComponent;
 import com.intellij.openapi.extensions.PluginId;
+import org.wso2.lsp4intellij.IntellijLanguageClient;
 
 import java.io.File;
-import java.lang.reflect.Field;
 
 public class AnalyticsPreloadActivity implements BaseComponent {
+  private static final String[] EXTENSIONS = {"xml", "json", "txt"};
+
   @Override
   public void initComponent() {
-    hackClassLoader();
     IdeaPluginDescriptor descriptor = PluginManager.getPlugin(PluginId.getId("org.jboss.tools.intellij.analytics"));
     File serverPath = new File(descriptor.getPath(), "lib/server/server.js");
-    LanguageServerDefinition.register(new AnalyticsLanguageServerDefinition("xml;json;txt", "node", new String[] { serverPath.getAbsolutePath().toString(), "--stdio"}));
-  }
-
-  private void hackClassLoader() {
-    ClassLoader loader = AnalyticsPreloadActivity.class.getClassLoader();
-    if (loader instanceof PluginClassLoader) {
-      try {
-        Field parentsField = loader.getClass().getDeclaredField("myParents");
-        parentsField.setAccessible(true);
-        ClassLoader[] parents = (ClassLoader[]) parentsField.get(loader);
-        if (parents.length > 1) {
-          ClassLoader first = parents[0];
-          parents[0] = parents[parents.length - 1];
-          parents[parents.length - 1] = first;
-        }
-      } catch (NoSuchFieldException | IllegalAccessException e) {
-        e.printStackTrace();
-      }
+    final String[] cmds = {"node", serverPath.getAbsolutePath().toString(), "--stdio"};
+    for(String ext : EXTENSIONS) {
+      AnalyticsLanguageServerDefinition serverDefinition = new AnalyticsLanguageServerDefinition(ext, cmds);
+      IntellijLanguageClient.addServerDefinition(serverDefinition);
+      IntellijLanguageClient.addExtensionManager(ext, serverDefinition);
     }
   }
 }
