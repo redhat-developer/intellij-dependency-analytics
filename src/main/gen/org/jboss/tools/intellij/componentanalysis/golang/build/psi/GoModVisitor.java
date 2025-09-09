d5af0f561c8bb02d3b807daed1c1c564c0eeb24f
Chao Wang <chaowan@redhat.com>
2025-09-09 13:12:50 +0800

    feat: Go Plugin dependency removal
    Signed-off-by: Chao Wang <chaowan@redhat.com>

---
+++ src/main/gen/org/jboss/tools/intellij/componentanalysis/golang/build/psi/GoModVisitor.java
@@ -0,0 +1,66 @@
+// This is a generated file. Not intended for manual editing.
+package org.jboss.tools.intellij.componentanalysis.golang.build.psi;
+
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiElement;
+
+public class GoModVisitor extends PsiElementVisitor {
+
+  public void visitExcludeBlock(@NotNull GoModExcludeBlock o) {
+    visitPsiElement(o);
+  }
+
+  public void visitExcludeSpec(@NotNull GoModExcludeSpec o) {
+    visitPsiElement(o);
+  }
+
+  public void visitExcludeStatement(@NotNull GoModExcludeStatement o) {
+    visitPsiElement(o);
+  }
+
+  public void visitGoStatement(@NotNull GoModGoStatement o) {
+    visitPsiElement(o);
+  }
+
+  public void visitItem(@NotNull GoModItem o) {
+    visitPsiElement(o);
+  }
+
+  public void visitModuleStatement(@NotNull GoModModuleStatement o) {
+    visitPsiElement(o);
+  }
+
+  public void visitReplaceBlock(@NotNull GoModReplaceBlock o) {
+    visitPsiElement(o);
+  }
+
+  public void visitReplaceSpec(@NotNull GoModReplaceSpec o) {
+    visitPsiElement(o);
+  }
+
+  public void visitReplaceStatement(@NotNull GoModReplaceStatement o) {
+    visitPsiElement(o);
+  }
+
+  public void visitRequireBlock(@NotNull GoModRequireBlock o) {
+    visitPsiElement(o);
+  }
+
+  public void visitRequireSpec(@NotNull GoModRequireSpec o) {
+    visitPsiElement(o);
+  }
+
+  public void visitRequireStatement(@NotNull GoModRequireStatement o) {
+    visitPsiElement(o);
+  }
+
+  public void visitStatement(@NotNull GoModStatement o) {
+    visitPsiElement(o);
+  }
+
+  public void visitPsiElement(@NotNull PsiElement o) {
+    visitElement(o);
+  }
+
+}
--- src/main/java/org/jboss/tools/intellij/componentanalysis/golang/GoCAIntentionAction.java
+++ src/main/java/org/jboss/tools/intellij/componentanalysis/golang/GoCAIntentionAction.java
@@ -11,11 +11,11 @@
 
 package org.jboss.tools.intellij.componentanalysis.golang;
 
-import com.goide.vgo.mod.psi.VgoModuleSpec;
-import com.goide.vgo.mod.psi.impl.VgoElementFactory;
 import com.intellij.codeInsight.intention.FileModifier;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.redhat.exhort.api.v4.DependencyReport;
@@ -24,18 +24,33 @@ import org.jboss.tools.intellij.componentanalysis.VulnerabilitySource;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class GoCAIntentionAction extends CAIntentionAction {
+    private static final Pattern VERSION_PATTERN = Pattern.compile("(\\s+)(v?[0-9]+\\.[0-9]+\\.[0-9]+\\S*)");
+
     GoCAIntentionAction(PsiElement element, VulnerabilitySource source, DependencyReport report) {
         super(element, source, report);
     }
 
     @Override
     protected void updateVersion(@NotNull Project project, Editor editor, PsiFile file, String version) {
-        VgoModuleSpec spec = (VgoModuleSpec) element;
-        String identifier = spec.getIdentifier().getText();
-        VgoModuleSpec newSpec = VgoElementFactory.createModuleSpec(project, identifier + " " + version);
-        if (newSpec != null) {
-            element.replace(newSpec);
+        Document document = PsiDocumentManager.getInstance(project).getDocument(file);
+        if (document == null) return;
+
+        // Find the line containing our element
+        int offset = element.getTextOffset();
+        int lineNumber = document.getLineNumber(offset);
+        int lineStart = document.getLineStartOffset(lineNumber);
+        int lineEnd = document.getLineEndOffset(lineNumber);
+        String lineText = document.getText().substring(lineStart, lineEnd);
+
+        // Replace the version in the line
+        Matcher matcher = VERSION_PATTERN.matcher(lineText);
+        if (matcher.find()) {
+            String newLineText = lineText.substring(0, matcher.start(2)) + version + lineText.substring(matcher.end(2));
+            document.replaceString(lineStart, lineEnd, newLineText);
         }
     }
 
